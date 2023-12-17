10b88c0a9e372bf69e9a6a8d0a8aa559f049451d
Laraib <laraibarjamand@gmail.com>
2023-12-17 14:54:34 +0500

    fix mysql dependency for sqlalchemy

--- taskapp/app.py
+++ taskapp/app.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# connect app to database
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+mysqlconnector://root:root@db:3306/taskapp")
+connection = engine.connect()
+
+# register routes
 from blueprints import auth_bp
 
 app.register_blueprint(auth_bp, url_prefix='/')
