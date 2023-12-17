85b520ddb0001f2a632f4400d18fce63982ac01c
Laraib <laraibarjamand@gmail.com>
2023-12-17 15:29:58 +0500

    use flask_sqlalchemy instead of sqlalchemy

--- taskapp/app.py
+++ taskapp/app.py
@@ -1,12 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# connect app to database
-from sqlalchemy import create_engine
-
-engine = create_engine("mysql+mysqlconnector://root:root@db:3306/taskapp")
-connection = engine.connect()
+# configure mysql database
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:root@db/taskapp"
+db = SQLAlchemy(app)
 
 # register routes
 from blueprints import auth_bp
