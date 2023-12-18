41e248e439b135a2a161301d4970a3b37b5b4788
Laraib <laraibarjamand@gmail.com>
2023-12-18 17:23:20 +0500

    project restructuring

--- taskapp/app.py
+++ taskapp/app.py
@@ -1,34 +1,29 @@
-from os import environ
-
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from taskapp.config import Config
 
 app = Flask(__name__)
+config = Config()
 
-# configure mysql database
-app.config["SQLALCHEMY_DATABASE_URI"] = environ.get("DATABASE_URI")
-app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
+app.config["SECRET_KEY"] = config.SECRET_KEY
+
 db = SQLAlchemy(app)
-
-# instantiate package for encryption
 bcrypt = Bcrypt(app)
 
-# add config for flask_login
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 
 
-# setup database tables
-from .models import Task, User
+from taskapp.models import Task, User
 
 with app.app_context():
     db.create_all()
 
-# register routes
-from . import views
+from taskapp import views
 
 app.add_url_rule("/register", view_func=views.UserRegisterView.as_view("register"))
 app.add_url_rule("/login", view_func=views.UserLoginView.as_view("login"))
